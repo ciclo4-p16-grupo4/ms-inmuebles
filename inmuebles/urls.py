17004382e6e87fdee4821d9a770ee0bd0fe31bb6
yaderr <yadersonjesuspatinovidal@gmail.com>
2021-11-19 20:18:49 -0500

    removed unused imports

--- inmuebles/urls.py
+++ inmuebles/urls.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from inmueblesApp import views as inmueblesAppViews
-from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
